9ea11288526f9a3b8d376c49dbc39fa4c216c025
Jay Hannes <Hannes_E@gmx.de>
2015-01-30 11:17:37 +0100

    Use gulp-watch instead of gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,16 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var watch = require('gulp-watch');
 
 gulp.task('connect', function() {
   connect.server({
     root: 'dist',
-    livereload: true
+    livereload: true,
   });
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./dist/*.*'], ['reload']);
-});
-
-gulp.task('reload', function() {
-  gulp.src('./dist/*.*')
-      .pipe(connect.reload());
+  watch('./dist/*').pipe(connect.reload());
 });
 
 gulp.task('default', ['connect', 'watch']);
